645c6ffc8d79bdcd55832817d9179a9952da5239
tjarbo <16938041+tjarbo@users.noreply.github.com>
2020-11-29 22:19:55 +0100

    :sparkles: Added watchtower

---
+++ Datenübertragung/blueprint/watchtower.py
@@ -0,0 +1,21 @@
+from .resources.fetch import FlightplansFetch, ItFetch, RadarFetch, TerminalFetch
+from .resources.utils import loggerFile
+class WatchTower():
+
+    def __init__(self):
+        self.flightplansFetch = FlightplansFetch()
+        self.itFetch = ItFetch()
+        self.radarFetch = RadarFetch()
+        self.terminalFetch = TerminalFetch()
+
+        loggerFile.log("WatchTower has been initalized!")
+
+    def watch(self):
+        loggerFile.log("Run WatchTower.watch ...")
+        self.flightplansFetch.fetch()
+        self.itFetch.fetch()
+        self.radarFetch.fetch()
+        self.terminalFetch.fetch()
+
+
+        
