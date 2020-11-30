d3e4bce3db62ed5b14eeda4a7263840c03a8f0d1
Silas <mail@silaspohl.de>
2020-11-30 09:47:32 +0100

    removed node_modules 1/2

--- GUI/react/src/api.js
+++
@@ -1,9 +0,0 @@
-import openSocket from 'socket.io-client';
-
-const socket = openSocket('http://localhost:5000/socket.io');
-
-function subscribeToEvent(cb) {
-    socket.on('event', EventData => cb(null, EventData));
-};
-
-export { subscribeToEvent };
